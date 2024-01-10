d21d9250617a2a02682b8c6986594021b008f0a5
protoN <103268792+oktayc0@users.noreply.github.com>
2024-01-10 18:59:44 +0300

    test.php
    Function tester php file

--- test.php
+++ test.php
@@ -1,8 +1,11 @@
 <?php
-$AnaResimYolu = 'img/test.png';    # Import image file
-$KayitKlasoru = 'img/iParts'       # Output image path
-$GenislikParcaSayisi = 4           #
-$YukseklikParcaSayisi = 2          #
+
+include("function.php"); # Call php image split function file
+
+$AnaResimYolu = 'img/test.png';   # Import image file
+$KayitKlasoru = 'img/iParts'      # Output image path
+$GenislikParcaSayisi = 4;         #
+$YukseklikParcaSayisi = 2;        #
 
 $Output = ResimAyir($AnaResimYolu,$KayitKlasoru,$GenislikParcaSayisi,$YukseklikParcaSayisi);
 
