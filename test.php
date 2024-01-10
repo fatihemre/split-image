a25378dda75b19a1242b242175ce6d79319a1e83
protoN <103268792+oktayc0@users.noreply.github.com>
2024-01-10 19:04:08 +0300

    test.php
    Image splitting function test page.

--- test.php
+++ test.php
@@ -9,12 +9,30 @@ $YukseklikParcaSayisi = 2;        #
 
 $Output = ResimAyir($AnaResimYolu,$KayitKlasoru,$GenislikParcaSayisi,$YukseklikParcaSayisi);
 
+echo '<pre>';
 Print_r($Output);
+echo '</pre>';
+
+
+
+
 
 # If everything goes well, the output should be like this:
-#
-# array(
-#
-#
+/*
+
+Array
+(
+    [0] => img/iParts/1704902429_0.png     # Image file 1
+    [1] => img/iParts/1704902429_1.png     # Image file 2
+    [2] => img/iParts/1704902429_2.png     # Image file 3
+    [3] => img/iParts/1704902429_3.png     # Image file 4
+    [4] => img/iParts/1704902429_4.png     # Image file 5
+    [5] => img/iParts/1704902429_5.png     # Image file 6
+    [6] => img/iParts/1704902429_6.png     # Image file 7
+    [7] => img/iParts/1704902429_7.png     # Image file 8
+    [KesiSayisi] => 8                      # Total number of divided images
+)
+
+*/
 
 ?>
